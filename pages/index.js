ae0fd43924e70a7b0797d5d2771bcca0814284d6
Grace <gsj33@cornell.edu>
2025-08-28 13:01:40 -0400

    vercel routing

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Cornell Sailing to the Stars</h1>
+            <p>Welcome to our website!</p>
+        </div>
+    )
+}
