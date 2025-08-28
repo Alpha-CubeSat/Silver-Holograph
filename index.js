2eb3bd3ee8291d46d2f2903c3f061624e20d4d29
Grace <gsj33@cornell.edu>
2025-08-28 13:26:54 -0400

    fix

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.use(express.static("public"));
 app.use(express.json()); //can add limits her in props to protect server
 
 
+app.get("/", (request, response) => {
+  response.send(`
+    <h1>Cornell Sailing to the Stars</h1>
+    <p>Welcome to our website!</p>
+    <p>API endpoint available at <a href="/api">/api</a></p>
+  `);
+});
+
 app.post("/api", (request, response) => {
   response.json({
     "votestatus": "successful!"
